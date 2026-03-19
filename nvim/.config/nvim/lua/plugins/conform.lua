48dd0193dadcaee85fa1319fa73c8686846e21cc
Jordan Price <jprice@paramify.com>
2026-03-19 15:07:38 -0600

    Finish configuring conform

--- nvim/.config/nvim/lua/plugins/conform.lua
+++ nvim/.config/nvim/lua/plugins/conform.lua
@@ -2,9 +2,7 @@ return {
   "stevearc/conform.nvim",
   opts = {},
   config = function()
-    local conform = require("conform")
-
-    conform.setup({
+    require("conform").setup({
       formatters_by_ft = {
         typescript = { "prettier" },
         typescriptreact = { "prettier" },
@@ -17,13 +15,38 @@ return {
           prepend_args = { "--indent-type", "Spaces", "--indent-width", "2" },
         },
       },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return {
+          -- These options will be passed to conform.format()
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        }
+      end,
     })
-    require("conform").setup({
-      format_on_save = {
-        -- These options will be passed to conform.format()
-        timeout_ms = 500,
-        lsp_format = "fallback",
-      },
+
+    -- :FormatDisable
+    vim.api.nvim_create_user_command("FormatDisable", function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, {
+      desc = "Disable autoformat-on-save",
+      bang = true,
+    })
+
+    -- :FormatEnable
+    vim.api.nvim_create_user_command("FormatEnable", function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, {
+      desc = "Enable autoformat-on-save",
     })
   end,
 }
