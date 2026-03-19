6d867e4aabd6e9aa1010bafc1c66fc3f43806356
Jordan Price <jprice@paramify.com>
2026-03-19 14:39:29 -0600

    Update catppuccin config

--- nvim/.config/nvim/lua/plugins/catppuccin.lua
+++ nvim/.config/nvim/lua/plugins/catppuccin.lua
@@ -1,10 +1,11 @@
 return {
   "catppuccin/nvim",
   name = "catppuccin",
+  priority = 1000,
   config = function()
     -- Choose your flavor: latte, frappe, macchiato, mocha
     require("catppuccin").setup({ flavour = "macchiato" })
     -- Activate the colorscheme
-    vim.cmd("colorscheme catppuccin")
+    vim.cmd("colorscheme catppuccin-nvim")
   end,
 }
