ef054efcac0066c3d54fca9a3a6d6518e543d562
shakedown-street <pricejordan@outlook.com>
2026-03-20 08:19:51 -0600

    Disable netrw, show find_files by default, add keybind for diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -15,4 +15,19 @@ vim.opt.autoindent = true
 
 vim.opt.termguicolors = true
 
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Diagnostic open float" })
+
+-- disable netrw completely
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("config.lazy")
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    -- only run if no files were passed on the command line
+    if vim.fn.argc() == 0 and vim.fn.line2byte("$") == -1 then
+      require("telescope.builtin").find_files()
+    end
+  end,
+})
