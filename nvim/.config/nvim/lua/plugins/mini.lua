b5b6145238aeeb3aaf132048be3f9ec9f472f027
shakedown-street <pricejordan@outlook.com>
2026-04-11 01:42:54 -0600

    Add mini files, jump and pairs

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,7 +2,35 @@ return {
   "nvim-mini/mini.nvim",
   version = false,
   config = function()
+    require("mini.icons").setup()
+    require("mini.files").setup({
+      options = {
+        use_as_default_explorer = false,
+      },
+    })
+    require("mini.jump").setup()
+    require("mini.pairs").setup()
     require("mini.statusline").setup()
     require("mini.tabline").setup()
+
+    local function toggleMiniFiles(cwd)
+      local MiniFiles = require("mini.files")
+      if not MiniFiles.close() then
+        local buf_name = vim.api.nvim_buf_get_name(0)
+        if not cwd then
+          MiniFiles.open(buf_name ~= "" and buf_name)
+        else
+          MiniFiles.open()
+        end
+      end
+    end
+
+    vim.keymap.set("n", "<leader>e", function()
+      toggleMiniFiles(true)
+    end, { desc = "MiniFiles.open cwd" })
+
+    vim.keymap.set("n", "<leader>E", function()
+      toggleMiniFiles(false)
+    end, { desc = "MiniFiles.open buffer" })
   end,
 }
