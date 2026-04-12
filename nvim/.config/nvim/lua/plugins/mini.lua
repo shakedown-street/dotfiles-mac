bf2dab7db2b7cd06db2629d95e76e592b70711df
shakedown-street <pricejordan@outlook.com>
2026-04-12 02:10:14 -0600

    Add cursorword and surround mini plugins

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -2,35 +2,27 @@ return {
   "nvim-mini/mini.nvim",
   version = false,
   config = function()
-    require("mini.icons").setup()
-    require("mini.files").setup({
-      options = {
-        use_as_default_explorer = false,
+    require("mini.cursorword").setup()
+    local files = require("mini.files")
+    files.setup({
+      windows = {
+        max_number = 3,
       },
     })
+    require("mini.icons").setup()
     require("mini.jump").setup()
     require("mini.pairs").setup()
     require("mini.statusline").setup()
+    require("mini.surround").setup()
     require("mini.tabline").setup()
 
-    local function toggleMiniFiles(cwd)
-      local MiniFiles = require("mini.files")
-      if not MiniFiles.close() then
-        local buf_name = vim.api.nvim_buf_get_name(0)
-        if not cwd then
-          MiniFiles.open(buf_name ~= "" and buf_name)
-        else
-          MiniFiles.open()
-        end
-      end
-    end
-
     vim.keymap.set("n", "<leader>e", function()
-      toggleMiniFiles(true)
-    end, { desc = "MiniFiles.open cwd" })
+      files.open()
+    end, { desc = "MiniFiles.open" })
 
     vim.keymap.set("n", "<leader>E", function()
-      toggleMiniFiles(false)
-    end, { desc = "MiniFiles.open buffer" })
+      local buf_name = vim.api.nvim_buf_get_name(0)
+      files.open(buf_name ~= "" and buf_name)
+    end, { desc = "MiniFiles.open at buffer" })
   end,
 }
